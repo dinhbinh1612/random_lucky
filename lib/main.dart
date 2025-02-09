e4461a43632a55ae92b1c5c5b84ccf6743dc624c
dinhbinh1612 <trinhcongdoanh204@gmail.com>
2025-02-09 23:20:01 +0700

    chooseMode

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:dart01/core/configs/theme/app_theme.dart';
+import 'package:dart01/presentation/choose_mode/bloc/theme_cubit.dart';
 import 'package:dart01/presentation/splash/pages/splash.dart';
 import 'package:device_preview_plus/device_preview_plus.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorageDirectory.web
+        : HydratedStorageDirectory((await getTemporaryDirectory()).path),
+  );
   runApp(DevicePreview(
     builder: (context) => MyApp(),
     enabled: true,
@@ -15,10 +26,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: AppTheme.lightTheme,
-      home: SplashPage(),
-      debugShowCheckedModeBanner: false,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => ThemeCubit(),
+        ),
+      ],
+      child: BlocBuilder<ThemeCubit, ThemeMode>(
+        builder: (context, mode) => MaterialApp(
+          theme: AppTheme.lightTheme,
+          darkTheme: AppTheme.darkTheme,
+          themeMode: mode,
+          home: SplashPage(),
+          debugShowCheckedModeBanner: false,
+        ),
+      ),
     );
   }
 }
--- lib/presentation/choose_mode/pages/choose_mode.dart
+++ lib/presentation/choose_mode/pages/choose_mode.dart
@@ -1,10 +1,143 @@
+import 'dart:ui';
+
+import 'package:dart01/common/widgets/button/basic_app_button.dart';
+import 'package:dart01/core/configs/assets/app_images.dart';
+import 'package:dart01/core/configs/assets/app_vectors.dart';
+import 'package:dart01/core/configs/theme/app_colors.dart';
+import 'package:dart01/presentation/choose_mode/bloc/theme_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class ChooseModePage extends StatelessWidget {
   const ChooseModePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.all(40),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(AppImages.chooseModeBG),
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.black.withValues(alpha: 220),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(40),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: SvgPicture.asset(AppVectors.logo),
+                ),
+                Spacer(),
+                Text(
+                  'Choose Mode',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        GestureDetector(
+                          onTap: () => context
+                              .read<ThemeCubit>()
+                              .updateTheme(ThemeMode.light),
+                          child: ClipOval(
+                            child: BackdropFilter(
+                              filter: ImageFilter.blur(
+                                sigmaX: 10,
+                                sigmaY: 10,
+                              ),
+                              child: Container(
+                                height: 80,
+                                width: 80,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color:
+                                      Color(0xff30393c).withValues(alpha: 128),
+                                ),
+                                child: SvgPicture.asset(
+                                  AppVectors.moon,
+                                  fit: BoxFit.none,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        Text(
+                          'Moon Mode',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: AppColors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(width: 30),
+                    Column(
+                      children: [
+                        GestureDetector(
+                          onTap: () => context
+                              .read<ThemeCubit>()
+                              .updateTheme(ThemeMode.dark),
+                          child: ClipOval(
+                            child: BackdropFilter(
+                              filter: ImageFilter.blur(
+                                sigmaX: 10,
+                                sigmaY: 10,
+                              ),
+                              child: Container(
+                                height: 80,
+                                width: 80,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color:
+                                      Color(0xff30393c).withValues(alpha: 128),
+                                ),
+                                child: SvgPicture.asset(
+                                  AppVectors.sun,
+                                  fit: BoxFit.none,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 15),
+                        Text(
+                          'Light Mode',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: AppColors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(height: 50),
+                BasicAppButton(onPressed: () {}, title: 'Continue')
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
